cf781f4b8782f0b40ea0f1cf0838c5ff4aed7cfa
loufangjin14052 <loufangjin14052@ipalfish.com>
2021-10-25 16:11:08 +0800

    feat: this在原型中的指向问题

--- 设计模式/js面试题/class1.js
+++ 设计模式/js面试题/class1.js
@@ -29,12 +29,24 @@ class Teacher extends Person {
 const student = new Student('学生', 10)
 const teacher = new Teacher('老师', '语文')
 // =>学生
-console.log(student.name)
-console.log(student.number)
-console.log(student.sayHi())
-console.log(student.score())
+// console.log(student.name)
+// console.log(student.number)
+// console.log(student.sayHi())
+// console.log(student.score())
 // ==>老师
-console.log(teacher.name)
-console.log(teacher.major)
-console.log(teacher.sayHi())
-console.log(teacher.teach())
+// console.log(teacher.name)
+// console.log(teacher.major)
+// console.log(teacher.sayHi())
+// console.log(teacher.teach())
+
+// ==> this的指向
+
+// 此时的this指向是函数的调用者也就是__proto__ 但是此时的student.__proto__是没有name的
+student.__proto__.sayHi()
+student.__proto__.name // undefined
+
+// ===> this的指向，函数的调用者student
+student.sayHi()
+// 类似
+student.__proto__.sayHi.call(student)
+
